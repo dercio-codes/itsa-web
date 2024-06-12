b8c266fbc07c07b9f87c61d42355a6b2a43ac381
ErronBlack <74609553+IamErronBlack@users.noreply.github.com>
2024-06-12 13:32:41 +0000

    added seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
+import { Helmet } from "react-helmet";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>ITSA - {"Home"}</title>
+        <meta
+          name="description"
+          content="ITSA is a platform for IT students at UFS to come together, fostering a culture of technological innovation and collaboration."
+        />
+        <meta
+          name="keywords"
+          content="ITSA, UFS, IT students, technological innovation, collaboration, Computer Science, Informatics, technology, innovation, students, association"
+        />
+      </Head>
+      <Helmet>
+        <meta property="og:title" content="ITSA - Home" />
+        <meta
+          property="og:description"
+          content="ITSA is a platform for IT students at UFS to come together, fostering a culture of technological innovation and collaboration."
+        />
+        <meta
+          property="og:image"
+          content="https://www.itsa-web.vercel.app/reps/2.png"
+        />
+      </Helmet>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
