cbffb28fb34aea14a3073fd699c08880ea7ee708
Dercio <76833905+dercio-codes@users.noreply.github.com>
2024-08-03 09:09:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2226,62 +2226,7 @@ export const ContactForm = () => {
   );
 };
 
-const reps = [
-  {
-    repName: "Hope",
-    repSurname: " Motchesane",
-    image: "/reps/Hope-Motchesane.jpg",
-    role: "President",
-  },
-    {
-    repName: "Ntokozo",
-    repSurname: " Langa",
-    role: "Vice-President",
-    image: "/reps/Ntokozo-Langa.jpg",
-  },
-  {
-    repName: "Kamohelo",
-    repSurname: " Motaung",
-    role: "Tresurer",
-    image: "/reps/Kamohelo-Motaung.jpg",
-  },
-  {
-    repName: "Lindele",
-    repSurname: " Nyambe",
-    image: "/reps/Lindele-Nyambe.jpg",
-    role: "Communications/Liason",
-  },
-    {
-    repName: "Yandile",
-    repSurname: " Myeni",
-    image: "/reps/Yandile-Myeni.jpg",
-    role: "Secretary",
-  },
-  {
-    repName: "Ntokozo",
-    repSurname: " Mashinini",
-    image: "/reps/Ntokozo-Mashinini.jpg",
-    role: "Database administrator",
-  },
-      {
-    repName: "Letseka",
-    repSurname: " Radebe",
-    image: "/reps/Letseka-Radebe.jpg",
-    role: "First-Year Representative",
-  },
-  {
-    repName: "Phenyo",
-    repSurname: " Manyathela",
-    image: "/reps/Phenyo-Manyathela.jpg",
-    role: "Additional Committee Member",
-  },
-  {
-    repName: "Simphiwe",
-    repSurname: " Mthembu",
-    image: "/reps/Simphiwe-Mthembu.jpg",
-    role: "Additional Committee Member",
-  },
-];
+
 
 const addtionalColors = {
   yellow: "#ffbe25",
