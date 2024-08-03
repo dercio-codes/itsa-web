7dec96d7dc7a1671f136e7bd322d0f4044d4d726
Dercio <76833905+dercio-codes@users.noreply.github.com>
2024-08-03 09:11:34 +0200

    Create reps.js

---
+++ app-data/reps.js
@@ -0,0 +1,56 @@
+export const reps = [
+  {
+    repName: "Hope",
+    repSurname: " Motchesane",
+    image: "/reps/Hope-Motchesane.jpg",
+    role: "President",
+  },
+    {
+    repName: "Ntokozo",
+    repSurname: " Langa",
+    role: "Vice-President",
+    image: "/reps/Ntokozo-Langa.jpg",
+  },
+  {
+    repName: "Kamohelo",
+    repSurname: " Motaung",
+    role: "Tresurer",
+    image: "/reps/Kamohelo-Motaung.jpg",
+  },
+  {
+    repName: "Lindele",
+    repSurname: " Nyambe",
+    image: "/reps/Lindele-Nyambe.jpg",
+    role: "Communications/Liason",
+  },
+    {
+    repName: "Yandile",
+    repSurname: " Myeni",
+    image: "/reps/Yandile-Myeni.jpg",
+    role: "Secretary",
+  },
+  {
+    repName: "Ntokozo",
+    repSurname: " Mashinini",
+    image: "/reps/Ntokozo-Mashinini.jpg",
+    role: "Database administrator",
+  },
+      {
+    repName: "Letseka",
+    repSurname: " Radebe",
+    image: "/reps/Letseka-Radebe.jpg",
+    role: "First-Year Representative",
+  },
+  {
+    repName: "Phenyo",
+    repSurname: " Manyathela",
+    image: "/reps/Phenyo-Manyathela.jpg",
+    role: "Additional Committee Member",
+  },
+  {
+    repName: "Simphiwe",
+    repSurname: " Mthembu",
+    image: "/reps/Simphiwe-Mthembu.jpg",
+    role: "Additional Committee Member",
+  },
+];
